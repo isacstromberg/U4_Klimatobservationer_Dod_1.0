f01ca0820a92851f278e33c877e11e27bd001ad5
Isac Strömberg <isacstromberg@hotmail.com>
2022-03-12 07:01:12 +0100

    Add project files.

---
+++ U4_Klimatobservationer_Dod_1.0/Repostories/Db_Repository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+
+namespace U4_Klimatobservationer_Dod_1._0.Repostories
+{
+    internal class Db_Repository
+    {
+        private string _connectionString;
+        public Db_Repository()
+        {
+            
+            var config = new ConfigurationBuilder().AddUserSecrets<Db_Repository>()
+                        .Build();
+
+            _connectionString = config.GetConnectionString("dbConn");
+        }
+
+
+    }
+}
